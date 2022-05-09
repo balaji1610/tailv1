1c9a8a853ff278fc75e836dd75a4caf43a55fc7e
balaji1610 <balajiece1610@gmail.com>
2022-05-09 14:40:07 +0530

    Container

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/img-redundant-alt */
 import logo from "./logo.svg";
 import "./App.css";
 <img src={logo} className="App-logo" alt="logo" />;
@@ -5,58 +6,45 @@ function App() {
   return (
     <div className="App">
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      <form class="flex items-center space-x-6">
-        <div class="shrink-0">
-          <img
-            class="h-16 w-16 object-cover rounded-full"
-            src="https://images.unsplash.com/photo-1580489944761-15a19d654956?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1361&q=80"
-            alt="Current profile photo"
-          />
-        </div>
-        <label class="block">
-          <span class="sr-only">Choose profile photo</span>
-          <input
-            type="file"
-            class="block w-full text-sm text-slate-500
-      file:mr-4 file:py-2 file:px-4
-      file:rounded-full file:border-0
-      file:text-sm file:font-semibold
-      file:bg-violet-50 file:text-violet-700
-      hover:file:bg-violet-100
-    "
-          />
-        </label>
-      </form>
 
-      <table class="border-collapse border border-slate-400 ...">
-        <thead>
-          <tr>
-            <th class="border border-slate-300 ...">State</th>
-            <th class="border border-slate-300 ...">City</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td class="border border-slate-300 ...">Indiana</td>
-            <td class="border border-slate-300 ...">Indianapolis</td>
-          </tr>
-          <tr>
-            <td class="border border-slate-300 ...">Ohio</td>
-            <td class="border border-slate-300 ...">Columbus</td>
-          </tr>
-          <tr>
-            <td class="border border-slate-300 ...">Michigan</td>
-            <td class="border border-slate-300 ...">Detroit</td>
-          </tr>
-        </tbody>
-      </table>
-      <p class="text-right ">So I started to walk into the water...</p>
-      <div class="flex flex-row">
-        <div class="basis-1/4">01</div>
-        <div class="basis-1/4">02</div>
-        <div class="basis-1/2">03</div>
+      <hr />
+      <div className="p-20 border-8 bg-purple-400 border-orange-100 hover:py-8 hover:bg-red-500">
+        <p>Padding</p>
       </div>
-    </div>
+      <section>
+        <div className="float-left m-7 inline	">
+          {" "}
+          <h1>Container</h1>
+        </div>
+      </section>
+      <section>
+        {" "}
+        <div class="card">
+          <p>Card Title</p>
+        </div>
+      </section>
+      <div class="columns-12 bg-green-400 border-4">
+        <p>Columns 1</p>
+        <p>Columns 2</p>
+        <p>Columns 3</p>
+        <p>Columns 4</p>
+        <p>Columns 5</p>
+        <p>Columns 6</p>
+        <p>Columns 7</p>
+        <p>Columns 8</p>
+        <p>Columns 9</p>
+        <p>Columns 10</p>
+        <p>Columns 11</p>
+        <p>Columns 12</p>
+      </div>
+      <hr />
+      <div class="columns-2">
+        <p>Well, let me tell you something, ...</p>
+        <p class="break-after-all">Sure, go ahead, laugh...</p>
+        <p>Maybe we can live without...</p>
+        <p>Look. If you think this is...</p>
+      </div>
+    </div> // end
   );
 }
 
